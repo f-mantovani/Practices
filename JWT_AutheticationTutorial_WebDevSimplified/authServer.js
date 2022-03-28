f4487d50c8c11590ef985a1f7743954475363ab6
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-03-29 00:25:58 +0200

    Finished the tutorial

---
+++ JWT_AutheticationTutorial_WebDevSimplified/authServer.js
@@ -0,0 +1,43 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const jwt = require("jsonwebtoken");
+
+app.use(express.json());
+
+//Just for tutorial purpose - Not even close of a good idea
+let refreshTokens = [];
+
+app.post("/token", (req, res) => {
+  const refreshToken = req.body.token;
+  if (refreshToken === null) return res.sendStatus(401);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = generateAccessToken({ name: user.name });
+    res.json({ accessToken });
+  });
+});
+
+app.delete("/logout", (req, res) => {
+  refreshTokens = refreshTokens.filter((token) => token !== req.body.token);
+  res.sendStatus(204);
+});
+
+app.post("/login", (req, res) => {
+  //Authenticate the User
+  const username = req.body.username;
+  const user = { name: username };
+  const accessToken = generateAccessToken(user);
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+  refreshTokens.push(refreshToken);
+  res.json({ accessToken, refreshToken });
+});
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15s" });
+}
+
+app.listen(4000, () => {
+  console.log(`Server Running on 4000`);
+});
--- JWT_AutheticationTutorial_WebDevSimplified/server.js
+++ JWT_AutheticationTutorial_WebDevSimplified/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
 const jwt = require("jsonwebtoken");
 
@@ -9,14 +10,28 @@ const posts = [
   { username: "Jim", title: "post 2" },
 ];
 
-app.get("/posts", (req, res) => {
-  res.json(posts);
+app.get("/posts", authenticateToken, (req, res) => {
+  res.json(posts.filter((post) => post.username === req.user.name));
 });
 
 app.post("/login", (req, res) => {
-  //Autheticate the User
+  //Authenticate the User
   const username = req.body.username;
+  const user = { name: username };
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken });
 });
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token === null) return res.sendStatus(401);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
 app.listen(3000, () => {
   console.log(`Server Running on 3000`);
